7457414074e7adb16f1d092bcc70924364914440
Sikan Li <sli4@staff.frontera.tacc.utexas.edu>
2024-07-08 17:44:33 -0500

    update container

--- scaling_part3/run_distributed.sh
+++ scaling_part3/run_distributed.sh
@@ -13,7 +13,7 @@ else
 fi
 
 
-PRELOAD="/opt/apps/tacc-apptainer/1.1.8/bin/apptainer exec --nv --bind /run/user:/run/user /scratch1/07980/sli4/containers/cnn_course.sif "
+PRELOAD="/opt/apps/tacc-apptainer/1.1.8/bin/apptainer exec --nv --bind /run/user:/run/user /scratch1/07980/sli4/containers/cnn_course_latest.sif "
 CMD="torchrun --nproc_per_node 4 --nnodes $NNODES --node_rank=$LOCAL_RANK --master_addr=$MAIN_RANK --master_port=1234 torch_train_distributed.py $@"
 
 FULL_CMD="$PRELOAD $CMD"
